404178500980b7bb1ed4505d998cfb923f812776
muhsaputra <muhsaputrabiz@gmail.com>
2026-02-22 11:35:22 +0700

    update tex

--- client/src/components/CheckStatusPage.jsx
+++ client/src/components/CheckStatusPage.jsx
@@ -251,7 +251,7 @@ const CheckStatusPage = () => {
                     <span className="text-[8px] md:text-[9px] font-black uppercase opacity-60 tracking-[0.2em] mb-1 block">
                       BIB Number
                     </span>
-                    <p className="text-3xl md:text-5xl font-black font-serif leading-none tracking-tighter">
+                    <p className="text-3xl md:text-2xl font-black font-serif leading-none tracking-tighter">
                       #{result.bibNumber || "---"}
                     </p>
                   </div>
@@ -263,7 +263,7 @@ const CheckStatusPage = () => {
                     <span className="text-[8px] md:text-[9px] font-black uppercase opacity-60 tracking-[0.2em] mb-1 block">
                       Jersey Size
                     </span>
-                    <p className="text-xl md:text-2xl font-black uppercase leading-none">
+                    <p className="text-3xl md:text-2xl font-black uppercase leading-none">
                       {result.jerseySize}
                     </p>
                   </div>
